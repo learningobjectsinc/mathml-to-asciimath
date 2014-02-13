7e7e9eec2cd414748f1b7bf3d384bea4994aafa3
Paul Salaets <psalaets@learningobjects.com>
2014-02-13 18:37:06 -0500

    tests for misc paren cases

--- test/test.js
+++ test/test.js
@@ -22,7 +22,9 @@ var testCases = [
   ['<math><mrow><mo>(</mo><mi>x</mi><mo>+</mo><mn>1</mn><mo>)</mo></mrow></math>', '( x + 1 )'],
   ['<math><mfrac><mrow><mi>x</mi><mo>+</mo><mn>1</mn></mrow><mn>4</mn></mfrac></math>', '( x + 1 ) / 4'],
   ['<math><mfrac><mrow><mn>1</mn><mo>+</mo><mn>2</mn><mo>+</mo><mn>3</mn><mo>+</mo><mn>4</mn></mrow><mrow><mn>10</mn><mo>+</mo><mi>a</mi><mo>+</mo><mi>b</mi></mrow></mfrac></math>', '( 1 + 2 + 3 + 4 ) / ( 10 + a + b )'],
-  ['<math><mn>4</mn><mfrac><mn>2</mn><mn>3</mn></mfrac></math>', '4 2 / 3']
+  ['<math><mn>4</mn><mfrac><mn>2</mn><mn>3</mn></mfrac></math>', '4 2 / 3'],
+  ['<math><mfrac><mn>3</mn><mn>4</mn></mfrac><mrow><mo>(</mo><mn>240</mn><mo>)</mo></mrow><mo>=</mo><mn>180</mn></math>', '3 / 4 ( 240 ) = 180'],
+  ['<math><mrow><mo>(</mo><mn>0.37</mn><mo>)</mo></mrow><mrow><mo>(</mo><mn>120</mn><mo>)</mo></mrow><mo>=</mo><mn>44.4</mn></math>', '( 0.37 ) ( 120 ) = 44.4']
 ];
 
 testCases.forEach(function(testCase) {
